7048da93225be01315de8b9d7edf9564109c3665
Aksels Ledins <aksels.ledins@gmail.com>
2017-02-01 23:58:46 +0100

    (main component) ProfesionnalExperience

---
+++ src/components/ProfessionalExperience/data/index.js
@@ -0,0 +1,7 @@
+
+export default [
+  { period: 'September 2016 - Today', company: 'Fizix.io', position: 'Full-Stack Javascript Developer' },
+  { period: 'March 2015 - August 2015', company: 'Creawave (Part of Acapela Group)', position: 'Software Engineer' },
+  { period: 'January 2014 - January 2015', company: 'IAO Media', position: 'Entrepreneur' },
+  { period: 'August 2013 - December 2013', company: 'IAO Media', position: 'FullStack Web Developer' },
+];
