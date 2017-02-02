62b3f45d9eea0cd9ad315365acf8f38a5b7f884f
aksels.ledins <aksels.ledins@gmail.com>
2017-02-02 11:54:06 +0100

    (generic component): Title > h2->h3

--- src/components/generic/Title/index.js
+++ src/components/generic/Title/index.js
@@ -4,7 +4,7 @@ import pure from 'recompose/pure';
 const Title = ({ title }) => (
 	<div className="row">
 		<div className="col-xs-12 col-lg-8 col-lg-offset-2">
-			<h2>{title}</h2>
+			<h3>{title}</h3>
 		</div>
 	</div>
 );
