cb991511f027f5e880bdb9d838c171acd2b12863
Aksels Ledins <aksels.ledins@gmail.com>
2017-02-05 21:43:21 +0100

    (client) add recaptcha

--- client/src/components/Contact/Form/index.js
+++ client/src/components/Contact/Form/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 import Error from '../../generic/Error';
 
@@ -9,26 +10,60 @@ class Form extends React.Component {
 
   state = {
     editable: true,
-    data: { name: '', email: '', message: '' },
+    data: { name: '', email: '', message: '', recaptchaValue: '' },
     error: '',
+    info: '',
   };
 
+  sendFormData = () => {
+
+    /* create request */
+    var x = new XMLHttpRequest();
+    var _this = this;
+
+    /* set the callback function */
+    x.onreadystatechange = function() {
+      if (x.readyState === 4) {
+        var response = JSON.parse(x.responseText);
+
+        if (x.status === 200) {
+          _this.setState({ info: 'Message sent !' });
+        }
+        else {
+          _this.setState({ error: response.msg });
+        }
+      }
+    }
+        /* send the request */
+    x.open('POST', 'http://localhost:7997/contact', true );
+    x.setRequestHeader('Content-type', 'application/json');
+
+    x.send( JSON.stringify( this.state.data ) );
+  }
+
   validate = (data) => {
-    if (!REGEX_EMAIL.test(this.state.email)) {
+    if (!this.state.data.recaptchaValue) {
+      this.setState({
+        editable: true,
+        error: 'Please fill the captcha',
+      });
+      return false;
+    }
+    if (!REGEX_EMAIL.test(this.state.data.email)) {
       this.setState({
         editable: true,
         error: 'Sorry it\'s not a valid email',
       });
       return false;
     }
-    if (this.state.name.length < 3) {
+    if (this.state.data.name.length < 3) {
       this.setState({
         editable: true,
         error: 'Name: should be at least 3 characters ',
       });
       return false;
     }
-    if (this.state.message.length < 4) {
+    if (this.state.data.message.length < 4) {
       this.setState({
         editable: true,
         error: 'Message: should be at least 4 characters',
@@ -39,11 +74,12 @@ class Form extends React.Component {
   }
 
   handleSubmit = (event) => {
-    this.setState({ editable: false });
+    this.setState({ editable: false, error: '' });
     event.preventDefault();
-    console.log('state', this.state);
+
     if (this.validate(this.state.data)) {
-      console.log('ok send message');
+      this.setState({ info: 'Sending message...', error: '' });
+      this.sendFormData();
     }
   }
 
@@ -55,6 +91,12 @@ class Form extends React.Component {
     }
   }
 
+  GRChange = (value) => {
+    this.setState(
+      { data: { ...this.state.data, recaptchaValue: value } }
+    );
+  }
+
   render() {
     return (
       <div className="col-xs-12 col-lg-8 col-lg-offset-2">
@@ -63,15 +105,26 @@ class Form extends React.Component {
 
         <form name="Form" onSubmit={this.handleSubmit}>
           {this.state.error && <Error data={this.state.error} /> }
+          {this.state.info &&
+            <p>
+              {this.state.info}
+            </p>
+          }
           <fieldset>
-            { require('util').inspect(this.state, { depth: null }) }
+            {/* require('util').inspect(this.state, { depth: null }) */}
             <label htmlFor="name">Name</label>
             <input type="text" name="name" value={this.state.data.name} onChange={this.handleChange} />
             <label htmlFor="email">Email</label>
             <input type="email" name="email" value={this.state.data.email} onChange={this.handleChange} />
             <label htmlFor="message">Message</label>
             <textarea name="message" value={this.state.data.message} onChange={this.handleChange} />
+            <ReCAPTCHA
+              ref="recaptcha"
+              sitekey="6LcBjR4TAAAAAPGttYPi7wVFO0FqYE4sREWlYinE"
+              onChange={this.GRChange}
+            />
             <input id="submit" type="submit" name="submit" value="Send Message" />
+
           </fieldset>
         </form>
 
