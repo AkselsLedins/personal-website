073541d9f501df7bcd5cadfc932fc61a3ff8707c
Aksels Ledins <aksels.ledins@gmail.com>
2017-02-06 21:46:16 +0100

    (client) Header > mobile fix

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import pure from 'recompose/pure';
 
+import $ from 'jquery';
+
 import Navigation from './Navigation';
+
+const handleClick = () => {
+	if ($('#side-nav').is(':hidden')) {
+		$('#side-nav').slideDown();
+		$('.menu-button').addClass('active');
+	} else {
+		$('#side-nav').slideUp();
+		$('.menu-button').removeClass('active');
+	}
+}
 
 const Header = () => (
 	<header>
 		{/* Container */}
 		<div className="container">
-			<div className="row header-inner">
+			<div className="row header-inner" style={{ paddingTop: 0 }}>
 
 				{/* Logo */}
 				<div className="col-xs-6 col-lg-2">
@@ -15,7 +27,7 @@ const Header = () => (
 				</div>
 
 				{/* Nav Button */}
-				<div className="menu-button">
+				<div className="menu-button" onClick={handleClick}>
 					<span className="before"></span>
 					<span className="main"></span>
 					<span className="after"></span>
